83ee051743568ca9ab26ca0f70cd311b5f4c3c95
K-Kizuku <kotani.kizuku@gmail.com>
2023-05-31 11:07:55 +0900

    [add] tailwindの導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,11 @@ import preact from "@astrojs/preact";
 import svelte from "@astrojs/svelte";
 import vue from "@astrojs/vue";
 import analogjsangular from "@analogjs/astro-angular";
-
 import alpinejs from "@astrojs/alpinejs";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), solidJs(), preact(), svelte(), vue(), analogjsangular(), alpinejs()]
+  integrations: [react(), solidJs(), preact(), svelte(), vue(), analogjsangular(), alpinejs(), tailwind()]
 });
